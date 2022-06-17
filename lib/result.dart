be2666ba6c3450c17ccfea20fbcaa6e1fd6b8ee7
med khalil <khalfaouimohamedkhalil@gmail.com>
2022-06-17 17:56:56 +0100

    v1

---
+++ lib/result.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  const Result(
+      {Key? key, required this.result, required this.isMale, required this.age})
+      : super(key: key);
+  final double result;
+  final bool isMale;
+  final int age;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Result'),
+      ),
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          children: [
+            Text(
+              'Gender : ${isMale ? 'Male' : 'Female'}',
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ), Text(
+              'Gender : ${isMale ? 'Male' : 'Female'}',
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ), Text(
+              'Gender : ${isMale ? 'Male' : 'Female'}',
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
